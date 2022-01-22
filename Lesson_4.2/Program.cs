a8701737923eb3cd49ffa8c75b1f36f8ca846787
Eugene <comradeeugene@gmail.com>
2022-01-22 22:00:19 +0300

    first

--- Lesson_4.2/Program.cs
+++ Lesson_4.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lesson_4
 {
@@ -6,7 +7,27 @@ namespace Lesson_4
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int MinValue = int.MaxValue;
+
+            WriteLine("Введите длинну последовательности");
+            int Length = int.Parse(ReadLine());
+
+            int[] arr = new int[Length];
+
+            for (int i = 0; i < Length; i++)
+            {
+                WriteLine($"Введите {i + 1}-й элемент последовательности");
+                arr[i] = int.Parse(ReadLine());
+            }
+            for (int i = 0; i < Length; i++)
+            {
+                if (arr[i] < MinValue)            
+                {
+                    MinValue = arr[i];
+                }
+            }
+            WriteLine($"Наименьшее число: {MinValue}");
+            ReadKey();
         }
     }
 }
